094ec27d38ae15f09493d73b8c72ec59fbf91ba6
gspeno <136727237+gspeno@users.noreply.github.com>
2024-12-31 14:21:31 -0400

    Add files via upload

---
+++ Fonctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventairePjt
+{
+    partial class Program
+    {
+        static void menu()
+        {
+            Console.WriteLine("\n=== MENU ===");
+            Console.WriteLine("1. Saisir les articles");
+            Console.WriteLine("2. Afficher tous les articles et leurs stocks");
+            Console.WriteLine("3. Calculer et afficher le prix total de l'inventaire");
+            Console.WriteLine("4. Trouver et afficher l'article le plus cher.");
+            Console.WriteLine("5. Rechercher un article par son nom et afficher son prix");
+            Console.WriteLine("6. Mettre à jour le stock d'un article");
+            Console.WriteLine("7. Afficher les articles en rupture de stock");
+            Console.WriteLine("8. Quitter");
+            Console.Write("Choisissez une option : ");
+       
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace inventairePjt
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration de variables
+            int choixMenu;
+
+            //Déclaration des tableaux grâce au nb d'articles saisis
+            string[] articles = new string[20];
+            int[] stocks = new int[20];
+
+            //Affichage du menu
+            menu();
+
+            //Saisie du choix de l'utilisateur
+            choixMenu = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
